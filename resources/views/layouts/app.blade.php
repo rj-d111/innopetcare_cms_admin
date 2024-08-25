834acdb614547d390dbe902ad70f252d16474d30
rj-d111 <raymondaguillar1@gmail.com>
2024-08-25 21:45:27 +0800

    third

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    @vite('resources/css/app.css')
-    @vite('resources/css/app.js')
+    {{-- @vite('resources/css/app.css') --}}
+    {{-- @vite('resources/css/app.js') --}}
+    <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 </head>
